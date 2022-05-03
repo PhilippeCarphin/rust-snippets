346eb55cdf02f5b4b5b0f495fe94920a07bcd538
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-05-03 08:42:57 -0400

    Added return type to main for errors

--- rs04-files/src/main.rs
+++ rs04-files/src/main.rs
@@ -5,35 +5,38 @@ use std::io::BufRead;
 use std::fs::File;
 use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), &'static str> {
     let argv: Vec<String> = env::args().collect();
     // let mut filename = argv.remove(1);
-    if argv.len() < 1 {
-        panic!("Need one argument");
+    if argv.len() <= 1 {
+        return Err("Need one argument")
     }
 
     /*
      * Probably what I would want to do most of the time if I were to code
      * a simple program in Rust.
      */
+    println!("=============== fs::read_to_string() ===============");
     if let Ok(contents) = fs::read_to_string(argv[1].clone()) {
-        println!("contents '{}'", contents);
+        println!("{}", contents);
     } else {
         println!("fs::read_to_string({}) returned an error value", argv[1]);
     }
-
     // And manipulate the string afterwards once the whole file has been read
 
     /*
      * Simplification of the below thing with their read_lines function into 
      * a single snippet.
      */
+    println!("======== File::open() + for (i,l) in BufReader::new(file).lines().enumerage() ... ===============");
     if let Ok(file) = File::open(argv[1].clone()){
         // let Ok(newvar) = X, if X is good, unwrap and put the good value in newvar
         let lines = io::BufReader::new(file).lines();
-        for line in lines {
+        // Also, check out this enumerate thing that gives you
+        // tuples containing indices and items
+        for (i, line) in lines.enumerate() {
             if let Ok(ip) = line {
-                println!("{}", ip);
+                println!("{}: {}", i+1, ip);
             }
         }
     } else {
@@ -47,17 +50,20 @@ fn main() {
      * is going on here and the code of the read_lines() function.
      */
     // From https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+    println!("======== read_lines() from doc.rust-lang.org  ===============");
     if let Ok(lines) = read_lines(argv[1].clone()) {
-        println!("Printing line by line using iterator as shown at doc.rust-lang.org");
         // Consumes the iterator, returns an (Optional) String
         for line in lines {
             if let Ok(ip) = line {
                 println!("{}", ip);
+            } else {
+                println!("Line not OK");
             }
         }
     } else {
         println!("read_lines({}) (from doc.rust-lang.org) returned an error thing", argv[1]);
     }
+    Ok(())
 
 }
 
@@ -65,7 +71,8 @@ fn main() {
 // The output is wrapped in a Result to allow matching on errors
 // Returns an Iterator to the Reader of the lines of the file.
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+    where P: AsRef<Path>
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
