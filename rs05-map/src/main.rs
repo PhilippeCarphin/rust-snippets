b3aadaa5df759b28f7ee9c965b2b83e689835541
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-05-04 23:01:33 -0400

    Hash map

---
+++ rs05-map/src/main.rs
@@ -0,0 +1,26 @@
+use std::collections::HashMap;
+
+fn main() {
+    // https://doc.rust-lang.org/book/ch08-03-hash-maps.html
+
+    let mut numbers = HashMap::new();
+
+    numbers.insert(String::from("twelve"), 12);
+    numbers.insert(String::from("five"), 5);
+
+    let mut letters = HashMap::new();
+
+    letters.insert(String::from("double you"), String::from("U"));
+    letters.insert(String::from("queue"), String::from("Q"));
+
+    println!("=========== letters ========================");
+    for (k,v) in letters {
+        println!("\x1b[1;31m{}\x1b[0m: \x1b[1;33m{}\x1b[0m", k, v);
+    }
+    // println!("letters = {}", letters);
+    println!("=========== numbers ========================");
+    println!("Hello, world!");
+    for (k,v) in numbers {
+        println!("{}: {}", k, v);
+    }
+}
