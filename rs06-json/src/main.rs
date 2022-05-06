4dfd668685172b3a5080260d6b900fbe0f0e6010
Philippe Carphin <philippe.carphin@polymtl.ca>
2022-05-06 13:10:46 -0400

    Example parsing JSON with known fixed keys

--- rs06-json/src/main.rs
+++ rs06-json/src/main.rs
@@ -1,3 +1,38 @@
+use serde::{Deserialize, Serialize};
+#[derive(Debug, Deserialize, Serialize)]
+struct Person {
+    name: String,
+    age: usize,
+    verified: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let json = r#"
+        {
+          "name": "George",
+          "age": 27,
+          "verified": false
+        }
+    "#;
+
+    let json_bad = r#"
+        {
+          "name": "George",
+          "age": 27,
+          "perified": false
+        }
+    "#;
+
+    let person: Result<Person,_> = serde_json::from_str(json);
+    if let Ok(person) = person {
+        println!("{:?}", person);
+    }
+
+    // let person_bad : Person = serde_json::from_str(json_bad).unwrap();
+    let person_bad: Result<Person,_> = serde_json::from_str(json_bad);
+    if let Ok(person) = person_bad {
+        println!("{:?}", person);
+    } else if let Err(e) = person_bad {
+        println!("Could not parse : {:?}", e);
+    }
 }
